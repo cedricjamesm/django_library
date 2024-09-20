d1930f0e13380fd88efd07dac97201219accaf58
Cedric Mapilisan <120065452+cedricjamesm@users.noreply.github.com>
2024-09-20 13:13:12 -0400

    Delete mapilisan_cedric_2220080 directory

--- mapilisan_cedric_2220080/mylibrary/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home-lib'),
-    path('author-list/', views.author, name='auth-lib'),
-    path('book-inst/<int:id>/', views.details, name='book-lib'),
-]
